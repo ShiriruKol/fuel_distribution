540a7aba825ba8fdfb2e206eb2af6fa9e87ef354
ShiriruKol <sakuradezkol@gmail.com>
2023-10-17 10:47:59 +0400

    Создание factory классов для автозаполнения значениями в бд

---
+++ database/factories/TypeFuelFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Fuel;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TypeFuel>
+ */
+class TypeFuelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'number_fuel' => fake()->randomFloat(2, 1, 2),
+            'fuel_id' => Fuel::get()->random()->id,
+            'user_id' => User::get()->random()->id,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Fuel;
+use App\Models\TypeFuel;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Fuel::factory(10)->create();
+        User::factory(5)->create();
+        TypeFuel::factory(15)->create();
     }
 }
