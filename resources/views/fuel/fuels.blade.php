22dfd7b4da12f549e2be655be98acede212f0b52
ShiriruKol <sakuradezkol@gmail.com>
2023-10-20 10:57:44 +0400

    Добавлена страниц, а также бэк с удалением вида топлива

--- resources/views/fuel/fuels.blade.php
+++ resources/views/fuel/fuels.blade.php
@@ -11,7 +11,7 @@
         <a href="{{route('fuels.create')}}" class="cursor-pointer focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Добавить</a>
     </div>
 
-    <div class="mt-4 grid grid-cols-2 md:grid-cols-3 grid grid-cols-4 gap-4">
+    <div class="mt-4 md:grid-cols-3 grid grid-cols-4 gap-4">
         @foreach($fuels as $fuel)
             <div class="text-center">
                 <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
