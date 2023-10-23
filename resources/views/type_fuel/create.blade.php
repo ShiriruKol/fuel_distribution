5c1d746aa5c50674bd06baaa73e60b3dbfc5e07b
ShiriruKol <sakuradezkol@gmail.com>
2023-10-23 14:08:02 +0400

    Обработка сохранения выбраного option в select, при неверно введенных данных

--- resources/views/type_fuel/create.blade.php
+++ resources/views/type_fuel/create.blade.php
@@ -26,7 +26,10 @@
                     <select id="user_id" name="user_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option selected>Выберите пользователя</option>
                         @foreach($users as $user)
-                            <option value={{ $user->id }}>{{ $user->name }}</option>
+                            @if($user->id == old('user_id'))
+                                <option value="{{ $user->id }}" selected>{{ $user->name }}</option>
+                            @endif
+                            <option value="{{ $user->id }}">{{ $user->name }}</option>
                         @endforeach
                     </select>
                     @error('user_id')
