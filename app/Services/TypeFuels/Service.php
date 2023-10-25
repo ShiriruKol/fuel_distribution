c628c9a2f0ecc71f242c428ab7d95d4a5269c627
ShiriruKol <sakuradezkol@gmail.com>
2023-10-25 17:19:10 +0400

    Добавление метода show

--- app/Services/TypeFuels/Service.php
+++ app/Services/TypeFuels/Service.php
@@ -22,4 +22,17 @@ class Service
         }
         return true;
     }
+
+    public function show()
+    {
+        $types = TypeFuel::where('status', 0)->get();
+        foreach ($types as $key => $type)
+        {
+            $user = DB::table('users')->select( 'name')->where('id', $type->user_id)->get();
+            $types[$key]['user_name'] = $user[0]->name;
+            $fuel = DB::table('fuels')->select( 'name')->where('id', $type->fuel_id)->get();
+            $types[$key]['fuel_name'] = $fuel[0]->name;
+        }
+        return $types;
+    }
 }
