ed22b6368b456ded0ca75084eeb196167dca98d4
ShiriruKol <sakuradezkol@gmail.com>
2023-10-27 10:40:15 +0400

    Добавление комментариев

--- app/Models/TypeFuel.php
+++ app/Models/TypeFuel.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @method static where(string $string, $id)
+ */
 class TypeFuel extends Model
 {
     use HasFactory;
